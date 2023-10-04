19b05f7ab16f92f51af20c5d4c55006f674b93cf
Muhammed Aslam Shah S <119589957+muhammedaslamshah@users.noreply.github.com>
2023-10-04 10:57:17 -0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Create account now")
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+print("Your account has been created successfully")
+print("login now!")
+
+username2 = input("Enter Username")
+password2 = input("Enter password")
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
+else:
+    print("Invalid Credentials")
